b370e5353732fe054c75096c91ad1fb1f7880682
Joseph C. Slater <joseph.c.slater@gmail.com>
2017-12-11 20:37:37 -0500

    brought to par with python module

--- so2ss.m
+++ so2ss.m
@@ -1,25 +1,34 @@
-function [a,b,c,d]=state(m,d,k,bs)
-% SO2SS [a,b,c,d]=STATE(M,C,K,Bs) returns the state matrix
+function [a,b,c,d]=so2ss(M, D, K, Bt, Cd, Cv, Ca)
+% SO2SS [a,b,c,d]=SO2SS(M, C, K, Bt, Cd, Cv, Ca) returns the state system
 % [xdot ]   [   0        I  ][ x  ]    [   0   ]
 % [     ] = [               ][    ] +  [       ] [u]
 % [xddot]   [-M^-1*K -M^-1*C][xdot]    [M^-1*Bs]
 %
-% from the originating equation
-% Mxddot+Cxdot+x=Bs u
+% y = C z + D u
 % 
-% An identity c matrix, and a zero d matrix are created.
+% where z = [x' xdot']'
+%
+% given the second order form equations
+% Mxddot+Cxdot+x=Bs u
+% y = Cd x + Cv xdot + Ca xddot
   
 % Copyright Joseph C. Slater, 2003
-if nargin==2
-  k=d;d=0*k;
-end
-l=length(diag(m));
-a=[zeros(l) eye(l);-m\k -m\d];
-len=size(m,1);
-if nargin==3
-  Bs=eye(len);
+% Updates Dec 11, 2017 to include Cd, Cv, and Ca.
+
+l=size(M,1);
+
+if nargin==4
+    Cd = eye(l)
+    Ca = zeros(l)
+    Cv = Ca
+if nargin<4
+  Bt=eye(l)  
+if nargin<3
+  K=D;D=0*K;
 end
 
-b=[zeros(len,size(Bs,2)); M\Bs];
-c=eye(2*len);
-d=zeros(size(C,1),size(B,2));
+a=[zeros(l) eye(l);-M\K -M\D];
+b=[zeros(l,size(Bt,2)); M\Bt];
+c=[Cd-Ca*M\K, Cv-Ca*M\D]
+d=Ca*M\Bt;
+
